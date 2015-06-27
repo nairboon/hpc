85abe0a300943451c76b8525a36164d82028f592
Remo hertig <remo.hertig@bluewin.ch>
2015-06-27 15:42:21 +0200

    prepare run script

--- run.sh
+++ run.sh
@@ -10,9 +10,21 @@ cd Output
 #../hcode/Src/hydro_omp -i ../hcode/Input/input_mini
 
 
-mpirun -n 2 ../hcode/Src/hydro_mpi -i ../hcode/Input/input_mini
 
 #LD_PRELOAD=/usr/lib/debug/usr/lib/valgrind/libmpiwrap-amd64-linux.so
 
 #mpirun -n 2 valgrind --leak-check=full --show-leak-kinds=all -v ../hcode/Src/hydro_mpi -i ../hcode/Input/input_mini
 
+
+
+if [ "$1" == "--cray" ]; then
+    ## on dora
+mpirun -n 2 ../hcode/Src/hydro_mpi -i ../hcode/Input/input_mini
+
+
+ else
+
+ ## at home
+
+mpirun -n 2 ../hcode/Src/hydro_mpi -i ../hcode/Input/input_mini
+fi
