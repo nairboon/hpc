de0e60f80c4ccb261ce701d2d773d7e1450e3874
Remo hertig <remo.hertig@bluewin.ch>
2015-06-29 01:07:57 +0200

    only 1 per numa

--- Experiment/strong_scale.sh
+++ Experiment/strong_scale.sh
@@ -65,6 +65,8 @@ echo "running with $1"
     if [ "$scenario" -eq "A" ]
    then
            aprun -n $1 -N $custom time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
+    elif [  "$scenario" -eq "T2" ]; then
+        aprun -n $1 -S 1 time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
 
         else
         aprun -n $1 time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
