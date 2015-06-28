e4bddeb1b228b401e42a5cc50f18429659eeeb3f
Remo hertig <remo.hertig@bluewin.ch>
2015-06-29 00:59:34 +0200

    some more data points

--- Experiment/launch_scenario_T2.sh
+++ Experiment/launch_scenario_T2.sh
@@ -8,10 +8,12 @@
 #SBATCH --error=hello_world_mpi-s.%j.err
 
 
-
+sbatch -A uzh8 strong_scale.sh --cray T2 400x1000 custom 2
 sbatch -A uzh8 strong_scale.sh --cray T2 700x1000 custom 2
 sbatch -A uzh8 strong_scale.sh --cray T2 800x1000 custom 2
 sbatch -A uzh8 strong_scale.sh --cray T2 900x1000 custom 2
 sbatch -A uzh8 strong_scale.sh --cray T2 1400x1000 custom 2
 sbatch -A uzh8 strong_scale.sh --cray T2 1800x1000 custom 2
 sbatch -A uzh8 strong_scale.sh --cray T2 2000x1000 custom 2
+sbatch -A uzh8 strong_scale.sh --cray T2 2400x1000 custom 2
+sbatch -A uzh8 strong_scale.sh --cray T2 2800x1000 custom 2
