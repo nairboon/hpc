f4e9fec1a68b622de68b46cad0f5ca5faa379d4b
Remo hertig <remo.hertig@bluewin.ch>
2015-06-29 19:52:42 +0200

    change starter a bit

--- Experiment/strong_scale.sh
+++ Experiment/strong_scale.sh
@@ -67,14 +67,14 @@ echo "running with $1"
            aprun -n $1 -N $custom time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
 
     elif [  "$scenario" -eq "T2" ]; then
-        aprun --ntasks=$1 -S 1 -ss --cpu_bind=sockets time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
+        aprun -N 1 --ntasks=$1 -S 1 -ss --cpu_bind=sockets time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
 
     elif [  "$scenario" -eq "T12" ]; then
-        aprun --ntasks=$1 -S 12 -ss -m plane=24:block --cpu_bind=cores time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
+        aprun -N 1 --ntasks=$1 -S 12 -ss -m plane=24:block --cpu_bind=cores time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
 
 
         elif [  "$scenario" -eq "S" ]; then
-        aprun --ntasks=$1 -S 12 -ss -m plane=24:block --cpu_bind=cores time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
+        aprun -N 22 --ntasks=$1 -S 12 -ss -m plane=24:block --cpu_bind=cores time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
         else
 
         aprun -n $1 time "$current/../hcode/Src/hydro_mpi" -i "$current/../hcode/Input/input_scenario_$2_$3"
