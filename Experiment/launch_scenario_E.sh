2c4dbcec544a6e8e3b3736cdcf3c6d67d207372a
Remo hertig <remo.hertig@bluewin.ch>
2015-06-30 12:00:32 +0200

    make scen smaler

--- Experiment/launch_scenario_E.sh
+++ Experiment/launch_scenario_E.sh
@@ -6,4 +6,4 @@
 #SBATCH --error=hello_world_mpi-s.%j.err
 
 
-sbatch --time=01:30:00 --job-name="scenario_E" --nodes=5 -A uzh8 strong_scale.sh --cray E a custom 112
+sbatch --time=03:30:00 --job-name="scenario_E" --nodes=5 -A uzh8 strong_scale.sh --cray E a custom 112
