26fe7f9077badc97775135cd7a9fda640ab9325f
Remo hertig <remo.hertig@bluewin.ch>
2015-06-30 11:25:54 +0200

    blabla

--- Experiment/launch_scenario_E.sh
+++ Experiment/launch_scenario_E.sh
@@ -6,4 +6,4 @@
 #SBATCH --error=hello_world_mpi-s.%j.err
 
 
-sbatch --time=01:30:00 --job-name="scenario_E" --nodes=1 -A uzh8 strong_scale.sh --cray E a custom 24
+sbatch --time=01:30:00 --job-name="scenario_E" --nodes=5 -A uzh8 strong_scale.sh --cray E a custom 112
